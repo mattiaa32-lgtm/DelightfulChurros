585339389a360b6f5789af97975e21f47f4682d9
mattiaa32-lgtm <mattiaa32@gmail.com>
2026-09-02 21:39:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    The cache name carries a version. Bump SW_VERSION when the shell
    changes; everything under an older name is deleted on activate. */
 
-var SW_VERSION = "v9";
+var SW_VERSION = "v10";
 var SHELL_CACHE = "shelf-shell-" + SW_VERSION;
 var MEDIA_CACHE = "shelf-media-" + SW_VERSION;
 
