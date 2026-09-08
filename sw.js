c40c6f51b274c4dd22a55f587a85fe5c8fa4ffbf
mattiaa32-lgtm <mattiaa32@gmail.com>
2026-09-08 09:03:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    The cache name carries a version. Bump SW_VERSION when the shell
    changes; everything under an older name is deleted on activate. */
 
-var SW_VERSION = "v68";
+var SW_VERSION = "v70";
 var SHELL_CACHE = "shelf-shell-" + SW_VERSION;
 var MEDIA_CACHE = "shelf-media-" + SW_VERSION;
 
@@ -41,6 +41,8 @@ var SHELL = [
   "js/arrivals.js",
   "js/gaps.js",
   "js/safety.js",
+  "js/shelfview.js",
+  "js/values.js",
   "js/panels.js",
   "js/settings.js",
   "js/ptr.js",
